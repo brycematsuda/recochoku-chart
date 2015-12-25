5477f576a527f94d7a9e117f161da814a78127fa
Bryce Matsuda <iexist4jcore@gmail.com>
2015-12-24 23:07:33 -1000

    Add functionality & tests for specifying a chart during initialization.

--- lib/chart.rb
+++ lib/chart.rb
@@ -1,9 +1,24 @@
 class Chart
   attr_reader :url
 
-  def initialize
+  def initialize(chart = nil)
       response = RestClient.get 'https://www.kimonolabs.com/api/49eulam0?apikey=' + ENV['NOKIRO_API']
       json = JSON.parse(response.body)
-      @url = json["results"]["charts"].first["link"]
+      idx = 0
+
+      case chart
+      when "jsd" # Daily singles
+        idx = 0
+      when "jsw" # Weekly singles
+        idx = 1
+      when "jad" # Daily albums
+        idx = 2
+      when "jaw" # Weekly albums
+        idx = 3
+      else
+        idx = 0 # Default: daily singles
+      end
+
+      @url = json["results"]["charts"][idx]["link"]
   end
 end
--- spec/chart_spec.rb
+++ spec/chart_spec.rb
@@ -3,13 +3,51 @@ require 'rest_client'
 require 'json'
 
 RSpec.describe Chart, "#url" do
-  context "no specified chart" do
-    it "returns the daily singles chart" do
-      response = RestClient.get 'https://www.kimonolabs.com/api/49eulam0?apikey=' + ENV['NOKIRO_API']
-      json = JSON.parse(response.body)
-      defaultUrl = json["results"]["charts"].first["link"]
+
+  before :all do
+    # Only call the API once
+    response = RestClient.get 'https://www.kimonolabs.com/api/49eulam0?apikey=' + ENV['NOKIRO_API']
+    @json = JSON.parse(response.body)
+  end
+
+  context "no chart specified" do
+    it "returns the daily singles chart url" do
+      defaultUrl = @json["results"]["charts"].first["link"]
       chart = Chart.new
       expect(chart.url).to eq defaultUrl
     end
   end
+
+  context "daily singles chart specified" do
+    it "returns the daily singles chart url" do
+      daily_singles_url = @json["results"]["charts"][0]["link"]
+      chart = Chart.new("jsd")
+      expect(chart.url).to eq daily_singles_url
+    end
+  end
+
+  context "weekly singles chart specified" do
+    it "returns the weekly singles chart url" do
+      weekly_singles_url = @json["results"]["charts"][1]["link"]
+      chart = Chart.new("jsw")
+      expect(chart.url).to eq weekly_singles_url
+    end
+  end
+
+  context "daily albums chart specified" do
+    it "returns the daily albums chart url" do
+      daily_album_url = @json["results"]["charts"][2]["link"]
+      chart = Chart.new("jad")
+      expect(chart.url).to eq daily_album_url
+    end
+  end
+
+  context "weekly albums chart specified" do
+    it "returns the weekly albums chart url" do
+      weekly_album_url = @json["results"]["charts"][3]["link"]
+      chart = Chart.new("jaw")
+      expect(chart.url).to eq weekly_album_url
+    end
+  end
+
 end
